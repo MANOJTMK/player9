a0ed2b1fa385baa96bdf6d2f6a2b468808bdc08b
MANOJTMK <46492877+MANOJTMK@users.noreply.github.com>
2019-03-09 12:59:50 +0530

    Create 81.c

---
+++ 81.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+    int n,m,a[100000],b[100000],i,j=0,l=0,k=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    scanf("%d",&a[i]);
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            l=a[i]&a[j];
+            if(l>k)
+            {
+                k=l;
+            }
+            
+        }
+    }
+    printf("%d",k);et
+    getch();
+}
